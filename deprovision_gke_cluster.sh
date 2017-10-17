2b634459b0319b6b8f170b734d5db2912107d7c0
ambarish2012 <ambarish@shippable.com>
2017-10-17 14:50:59 -0700

    update sample

--- deprovision_gke_cluster.sh
+++ deprovision_gke_cluster.sh
@@ -3,4 +3,5 @@
 
 # get the cluster name from central state
 cluster_name=$(shipctl get_resource_version_key "central_state" "cluster_name")
-gcloud container clusters delete $cluster_name
+zone=$(shipctl get_resource_version_key "central_state" "zone")
+gcloud container clusters delete $cluster_name --zone=$zone
--- provision_gke_cluster.sh
+++ provision_gke_cluster.sh
@@ -1,6 +1,6 @@
 # This script provisions a GKE cluster
 #! /bin/bash -e
 
-gcloud config set compute/zone us-central1-b
+gcloud config set compute/zone $2
 
 gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1
