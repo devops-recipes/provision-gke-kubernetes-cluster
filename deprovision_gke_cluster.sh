d27aeccfe86a3e174f10f37f65b3c7f22786c8bd
ambarish2012 <ambarish@shippable.com>
2017-10-17 15:03:52 -0700

    update sample

--- deprovision_gke_cluster.sh
+++ deprovision_gke_cluster.sh
@@ -1,7 +1,4 @@
 # This script deprovisions a GKE cluster
 #! /bin/bash -e
 
-# get the cluster name from central state
-cluster_name=$(shipctl get_resource_version_key "central_state" "cluster_name")
-zone=$(shipctl get_resource_version_key "central_state" "zone")
-gcloud container clusters delete $cluster_name --zone=$zone
+gcloud container clusters delete $1 --zone=$2
