96b75c9d3aa75a73179525c3c6ca37795fb82f50
ambarish2012 <ambarish@shippable.com>
2017-10-18 10:36:03 -0700

    update sample

--- deprovision_gke_cluster.sh
+++ deprovision_gke_cluster.sh
@@ -1,3 +1,18 @@
 # This script deprovisions a GKE cluster
 #! /bin/bash -e
-gcloud -q container clusters delete $1 --zone=$2
+
+# check if services are running and delete cluster only if no service is found
+
+# generate the kubectl config file
+gcloud container clusters get-credentials $1 --zone $2
+
+# replace service with your own service name
+service="hello-world"
+response=$(kubectl get service $service)
+echo "service query response "$response
+
+if [[ $response =~ .*(NotFound).* ]]
+then
+   echo "no service found, deleting cluster"
+   gcloud -q container clusters delete $1 --zone=$2
+fi
