5dd9acd68e7804591849ee5bd65a1bc45b16d3a8
ambarish2012 <ambarish@shippable.com>
2017-10-18 10:00:44 -0700

    update sample

--- deprovision_gke_cluster.sh
+++ deprovision_gke_cluster.sh
@@ -1,5 +1,3 @@
 # This script deprovisions a GKE cluster
 #! /bin/bash -e
-gcloud config set compute/zone us-central1-b
-
-gcloud -q container clusters delete $1 --zone=us-central1-b
+gcloud -q container clusters delete $1 --zone=$2
